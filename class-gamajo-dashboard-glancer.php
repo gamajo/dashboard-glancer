64d0a41bd46049fad035ae298947c069cbdbf39f
Gary Jones <GaryJones@users.noreply.github.com>
2014-02-17 15:58:22 +0000

    Use succinct code
    Tidy up b77def64925b02f1192ed85df482abbd3f13b171
    
    See #3

--- class-gamajo-dashboard-glancer.php
+++ class-gamajo-dashboard-glancer.php
@@ -144,11 +144,7 @@ class Gamajo_Dashboard_Glancer {
 	 */
 	protected function get_label( array $item, $count ) {
 		$post_type_object = get_post_type_object( $item['type'] );
-		if ( 1 === $count ) {
-		    $label = $post_type_object->labels->singular_name;	
-		} else {
-		    $label = $post_type_object->labels->name;
-		}
+		$label = 1 === $count ? $post_type_object->labels->singular_name : $post_type_object->labels->name;
 
 		// Append status for non-publish statuses for disambiguation
 		if ( 'publish' !== $item['status'] ) {
